a41331074a0956f58b090ef36d7aded7fb75c76d
王富欣 <apple1987red@163.com>
2021-05-17 00:22:38 +0800

    Add files via upload

---
+++ clashfreev2.ps1
@@ -0,0 +1,6 @@
+﻿$1=curl https://v2.kkpp.cc/users/pojiezhiyuanjun/events
+$2=$1.Content.IndexOf("created_at")+18
+$d=$1.Content[$2]+$1.Content[$2+1]+$1.Content[$2+3]+$1.Content[$2+4]
+$link="https://gh.haval.gq/pojiezhiyuanjun/freev2/raw/master/"+$d+"clash.yml"
+echo $link | clip
+echo $link 
